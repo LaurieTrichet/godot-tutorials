ee4cdcecafe93c9434dc91ba3e55f0bc1852599d
Laurie Trichet <laurie.trichet@protonmail.com>
2022-08-21 21:48:05 +1000

    add project files

---
+++ Mob.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite.playing = true
+	var mob_animations = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_animations[randi() % mob_animations.size()]
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
---
+++ Player.gd
@@ -0,0 +1,51 @@
+extends Area2D
+
+signal hit
+
+export var speed = 100
+var screen_size
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+#	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity += Vector2.RIGHT
+	if Input.is_action_pressed("move_left"):
+		velocity += Vector2.LEFT
+	if Input.is_action_pressed("move_up"):
+		velocity += Vector2.UP
+	if Input.is_action_pressed("move_down"):
+		velocity += Vector2.DOWN
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+		var animation_name = "walk" if not velocity.y != 0 else "up"
+		$AnimatedSprite.flip_h = velocity.x < 0
+		$AnimatedSprite.flip_v = velocity.y > 0
+			
+		$AnimatedSprite.play(animation_name)
+	else:
+		$AnimatedSprite.stop()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+
+func _on_Player_body_entered(_body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false 
